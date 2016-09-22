b05876673dd354ba0690dbb82b2f6e64251016ab
devjin0617 <devjin0617@gmail.com>
2016-09-22 22:06:37 +0900

    Add assets and project files

---
+++ Assets/auto.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var gameObject :  GameObject;
+
+function Start () {
+	//var gameObject = GameObject.FindWithTag("MainCamera");
+
+//	var object = GameObject.GetComponent(AnimationManagerUI) as AnimationManagerUI;
+
+// 	var object = gameObject.GetComponent(AnimationManagerUI);
+
+
+}
+
+function Update () {
+
+}
